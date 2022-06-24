1a87eda3264c89dc2ce515afa8bc427386e01f7e
Alexander Sallmann <a.sallmann00@gmail.com>
2022-06-24 15:21:23 +0200

    Updated index.html for frontend purposes and added status to MaintenanceMonitor (and MaintenanceController)

--- src/main/java/com/example/maintenancemonitor/MaintenanceMonitor.java
+++ src/main/java/com/example/maintenancemonitor/MaintenanceMonitor.java
@@ -3,6 +3,15 @@ package com.example.maintenancemonitor;
 public class MaintenanceMonitor {
 
     public String message = "test123";
+    public boolean status = true;
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
     public String getMessage(){
         return this.message;
